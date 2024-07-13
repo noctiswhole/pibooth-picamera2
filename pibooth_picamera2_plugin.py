c74f83621ef044ab6303e27507d175addcb07996
Tertese Samuel Moses <smatnemo25@gmail.com>
2024-07-13 09:46:09 +0100

    Moved pibooth_picamera2 to pibooth_picamera2_plugin

--- pibooth_picamera2_plugin.py
+++ pibooth_picamera2_plugin.py
@@ -4,29 +4,35 @@ except Exception as e:
     print(e)
     exit()
 from rpi_picamera2 import Rpi_Picamera2, get_rpi_picamera2_proxy
+from pibooth.utils import LOGGER
+
+
+# Release version
+__version__ = "1.0.0"
+
+@pibooth.hookimpl 
+def pibooth_configure(cfg):
+    """Declare new configuration options.
+    """
+    cfg.add_option('CAMERA','use_picamera2',1,
+                    "Boolean value to use Picamera2 library and the new raspberry pi camera v3")
 
 # This hook returns the custom camera proxy.
 # It is defined here because yield statement in a hookwrapper with a 
 # similar name is used to invoke it later. But check first if other cameras
 # are installed or if the user specifically asks for this
 @pibooth.hookimpl
-def pibooth_setup_camera():
-    rpi_picamera2_proxy = get_rpi_picamera2_proxy()
-    # print(rpi_picamera2_proxy)
-    # exit()
+def pibooth_setup_camera(cfg):
+    
+    rpi_picamera2_proxy = None
+    if cfg.get('CAMERA','use_picamera2'):
+        rpi_picamera2_proxy = get_rpi_picamera2_proxy()
+    
+    if not rpi_picamera2_proxy:
+        LOGGER.info('Could not find picamera2')
+        return
     return Rpi_Picamera2(rpi_picamera2_proxy) 
 
-class PiCamera2Plugin():
 
-    name = 'pibooth_picamera2:camera'
-
-    def __init__(self):
-        pass
     
-    # @pibooth.hookimpl
-    # def pibooth_setup_camera(self):
-    #     rpi_picamera2_proxy = get_rpi_picamera2_proxy()
-    #     # print(rpi_picamera2_proxy)
-    #     # exit()
-    #     return Rpi_Picamera2(rpi_picamera2_proxy)
     
